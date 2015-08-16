39605a8ca29e9b4bbd44a37880354b083b9afce8
Chuck Harmston <chuck@chuckharmston.com>
2015-08-16 11:42:44 -0600

    Adds Stylus, Nib, and Gulp command to compile and minify CSS.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,43 @@
+var concat = require('gulp-concat');
 var gulp = require('gulp');
 var gls = require('gulp-live-server');
+var minifyCss = require('gulp-minify-css');
+var nib = require('nib');
 var path = require('path');
+var stylus = require('gulp-stylus');
 
 
-var files = {
-  SERVER: path.join(__dirname, 'index.js')
+var PATHS = {
+  BUILD: {
+    CSS: path.join(__dirname, 'build', 'css')
+  },
+  SRC: {
+    CSS: path.join(__dirname, 'src', 'public', 'css'),
+    IMG: path.join(__dirname, 'src', 'public', 'img'),
+    JS: path.join(__dirname, 'src', 'public', 'js'),
+  },
+  SERVER: path.join(__dirname, 'index.js'),
 };
 
+
+gulp.task('css', function() {
+  gulp
+    .src(path.resolve(PATHS.SRC.CSS, '**/*.styl'))
+    .pipe(stylus({
+      compress: true,
+      use: [
+        nib()
+      ]
+    }))
+    .pipe(concat('bundle.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest(PATHS.BUILD.CSS));
+});
+
 gulp.task('server', function() {
-  var server = gls.new(files.SERVER);
+  var server = gls.new(PATHS.SERVER);
   server.start();
-  gulp.watch(files.SERVER, server.start.bind(server)); 
+  gulp.watch(PATHS.SERVER, server.start.bind(server)); 
 });
 
 gulp.task('default', ['server']);
